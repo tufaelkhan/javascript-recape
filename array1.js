1c4df9c1e3a5348e7ea70a229cb329f002e99020
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-10 11:16:10 -0800

    add array methods

---
+++ array1.js
@@ -0,0 +1,11 @@
+// includes() check specific value return boolean true/false
+const response = [ 'tufael', 'sujoy', 'sonjoy', 'mehedi', 'jahid']
+const result = response.includes('sonjoy')
+// console.log(result);
+
+// slice(), create a new array that contains a portion of an existing array
+const res = response.slice(0, 3)
+// console.log(res);
+
+const str = 'hello world';
+console.log(str.includes('w'));
