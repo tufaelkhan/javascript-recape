dcbbe9555e65918363e97a8d191943cdea0e1e1b
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-11 06:49:08 -0800

    add object

--- operators.js
+++ operators.js
@@ -7,12 +7,29 @@ const myArray = [num1[0], num1[1], num1[2] ]
 // console.log(myArray.push(num1));
 // console.log(num2);
 
-const myAddFunction = ( ...res) => {
+const myAddFunction = ( first, second, ...res) => {
+    console.log(first);
+    console.log(second);
     console.log(res);
+    let sum = 0
     res.forEach((num) => {
-        num = num + 2
-        console.log(num);
+        sum = sum + num
+        console.log(sum);
     })
+    console.log(sum);
 }
-myAddFunction(4, 6, 7,8, 7, 50, 4)
+// myAddFunction(4, 6, 7, 8, 7, 50, 4)
 // console.log(myAddFunction(4, 6, 7,8, 7, 50, 4));
+
+const student = {
+    name: 'tufael',
+    email: 'tufael@gmail.com',
+    id: 3434,
+    designation: 'web developer',
+}
+const teacher = {
+    ...student,
+    subject: 'ict',
+    company: 'daffodil',
+}
+console.log(teacher);
